348350c45f73a1bc1fd2eadf6c3ee1aa281f756d
Anton Kruglikov <90029783+KruglikovAnton@users.noreply.github.com>
2022-08-11 19:42:47 +0300

    Delete for_tests2.py

--- for_tests2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-from file_extracters import add_material_from_pdc_for_prediction
-
-X = pd.DataFrame(add_material_from_pdc_for_prediction('2021-09b.PDC_B0', materials=np.arange(15563, 50123)))
-
-
